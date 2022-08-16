b71b721d8b81a754488a0b3277923e1be9765ba7
Jeroen Boersma <JeroenBoersma@users.noreply.github.com>
2022-08-16 12:35:43 +0200

    Fixed weekly report

--- lib/harvest_notifier/templates/weekly_report.rb
+++ lib/harvest_notifier/templates/weekly_report.rb
@@ -8,7 +8,7 @@ module HarvestNotifier
       REMINDER_TEXT = "*Hey ho! Een aantal van jullie schatjes is vergeten de uren te boeken in Harvest.*"
       USERS_LIST_TEXT = "Zonder public te shamen :wink: hierbij een reminder voor mensen die vergeten zijn uren te boeken vorige week: *%<period>s*"
       REPORT_NOTICE_TEXT = "_Controleer nog even je uren en reageer met :heavy_check_mark: op dit bericht, druk op bijwerken als je vind dat je voldoende hebt gedaan om niet op het lijstje te staan :sweat_smile:_"
-      SLACK_ID_ITEM = "• <@%<slack_id>s>: *%<missing_hours>* uurtjes missen van de %<weekly_capacity>s"
+      SLACK_ID_ITEM = "• <@%<slack_id>s>: *%<missing_hours>s* uurtjes missen van de %<weekly_capacity>s"
       FULL_NAME_ITEM = "• %<full_name>s: *%<missing_hours>s* uren van %<weekly_capacity>s"
 
       def generate # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
